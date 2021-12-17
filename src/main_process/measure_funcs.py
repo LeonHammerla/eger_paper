1ab583b4c4d35e30dc49dc3c56b0d1c96153f036
LeonHammerla <leon.hammerla@gmx.de>
2021-12-17 15:05:26 +0100

    implemented select utility and measure-funcs

--- src/main_process/measure_funcs.py
+++ src/main_process/measure_funcs.py
@@ -6,24 +6,72 @@ sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '
 import cassis
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+VERB_TAGS = ["VAPP", "VAPS", "VMFIN", "VMIMP", "VMINF", "VMPP", "VMPS", "VVFIN", "VVIMP", "VVINF", "VVPP", "VVPS"]
 
 
-def verbs_per_sentence(cas: cassis.Cas) -> List[int]:
+def select_sentences_from_cas(cas:cassis.Cas) -> List[cassis.typesystem.FeatureStructure]:
+    """
+    Function returns all sentences from a cas-object.
+    :param cas:
+    :return:
+    """
+    sentences = cas.select("de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence")
+    return sentences
+
+
+def select_tokens_from_sentence_from_cas(cas: cassis.Cas, sentence: cassis.typesystem.FeatureStructure) -> List[cassis.typesystem.FeatureStructure]:
+    """
+    Funmction returns all token from a sentence from a cas-object
+    :param cas:
+    :param sentence:
+    :return:
+    """
+    tokens = cas.select_covered("de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token", sentence)
+    return tokens
+
+
+def verbs_per_sentence(cas: cassis.Cas,
+                       tokens: List[cassis.typesystem.FeatureStructure]) -> int:
     """
     Calculates how many verbs are in a sentence.
+    =========================== List of Verb-Tags ============================
+    VAPP	Auxiliar, Partizip Präteritum, im Verbalkomplex
+    VAPS	Auxiliar, Partizip Präsens, im Verbalkomplex
+    VMFIN	Modalverb, finit
+    VMIMP	Modalverb, Imperativ
+    VMINF	Modalverb, Infinitiv
+    VMPP	Modalverb, Partizip Präteritum, im Verbalkomplex
+    VMPS	Modalverb, Partizip Präsens, im Verbalkomplex
+    VVFIN	Vollverb, finit
+    VVIMP	Vollverb, Imperativ
+    VVINF	Vollverb, Infinitiv
+    VVPP	Partizip Präteritum, im Verbalkomplex
+    VVPS	Partizip Präsens, im Verbalkomplex
+    ===========================================================================
+    :param tokens:
     :param cas:
     :return:
     """
-    pass
+
+    verb_count = 0
+    for token in tokens:
+        pos = cas.select_covered("de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS", token)[0]
+        pos_value = pos["PosValue"]
+        for possible_verb_tag in VERB_TAGS:
+            if possible_verb_tag in pos_value:
+                verb_count += 1
+                break
+
+    return verb_count
 
 
-def token_per_sentence(cas: cassis.Cas) -> List[int]:
+def token_per_sentence(tokens: List[cassis.typesystem.FeatureStructure]) -> int:
     """
     Calculates how many tokens are in a sentence.
-    :param cas:
+    :param tokens:
     :return:
     """
-    pass
+    return len(tokens)
 
 
 def mdd_of_sent(cas: cassis.Cas) -> List[float]:
@@ -34,7 +82,6 @@ def mdd_of_sent(cas: cassis.Cas) -> List[float]:
     :param cas:
     :return:
     """
-
 
 def altmann(cas: cassis.Cas) -> List[float]:
     """
