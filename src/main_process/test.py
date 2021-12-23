cf13dd5312c919e6014ec4a4f2f9e46404ee4dad
LeonHammerla <leon.hammerla@gmx.de>
2021-12-23 18:31:09 +0100

    added function to just plot the result pickles in the results directory in data/corpus_ident

--- src/main_process/test.py
+++ src/main_process/test.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from multiprocessing import Pool
 from tqdm import tqdm
-
+import random
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
-data_dir = os.path.join(ROOT_DIR, "data")
+data_dir = os.path.join(ROOT_DIR, "data", "test")
 
-"""
+
 dir_path = os.path.join(data_dir, "COAH", "timeslices")
 #filepaths = [os.path.join(dir_path, filename) for filename in os.listdir(dir_path)]
 filepaths = ["hooboo/hbjkbuhk/ozunbou/res_929.pickle",
@@ -34,17 +34,36 @@ for i in range(0, 10, 2):
 print(type((2,2)))
 
 with PdfPages(os.path.join(data_dir, 'foo.pdf')) as pdf:
-    plt.semilogy()
-    df = pd.DataFrame({"h":[1.2, 1.2121212, 3], "x":[1,2,3]})
-    fig = df.plot(xlabel ='Time_Slices', ylabel='Token_per_sent', kind="box").get_figure()
+    #plt.semilogx()
+    flierprops = dict(marker='x', markersize=2,
+                      linestyle='none')
+    a = {}
+    for i in range(1900, 1960):
+        x = []
+        for j in range(0, 10):
+            x.append(random.randrange(0, 100))
+        a[str(i)] = x
+    #a["1939"][4] = 10000
+    df = pd.DataFrame(a)
+
+    fig = df.plot(xlabel='Time_Slices', ylabel='Token_per_sent', kind="box", fontsize=4, flierprops=flierprops, showfliers=True, rot=90).get_figure()
+
+    #plt.xticks(rotation=90)
+
+    #fig = df.plot(xlabel ='Time_Slices', ylabel='Token_per_sent', kind="box")
+    #[ax_tmp.set_xlabel('') for ax_tmp in np.asarray(fig).reshape(-1)]
+    #fig = np.asarray(fig).reshape(-1)[0].get_figure()
     pdf.savefig(fig)
 
     print(type(fig))
     df = pd.DataFrame({"h": [1.2, 1.2121212, 3,5,np.nan], "x": [1, 2, 3, 8, 9]})
     fig = df.plot(xlabel ='Time_Slices', ylabel='Token_per_sent', kind="box").get_figure()
+    #plt.xticks(rotation=90)
     pdf.savefig(fig)
 
 
+
+"""
 plt.semilogy()
 print(type(axxr))
 plt.show()
@@ -62,7 +81,9 @@ data = [d[m] for d, m in zip(data.T, mask.T)]
 plt.boxplot(data)
 
 plt.show()
-"""
+
+
+
 import time
 def f(i):
     time.sleep(i)
@@ -76,3 +97,4 @@ with pool as p:
 
 pool.close()
 pool.join()
+"""
